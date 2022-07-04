2b04735238adf2f682513522b23a451d882fc396
Max Kossatz <mkossatz@redhat.com>
2022-07-04 12:30:03 +0200

    fixed web-app file as Charlie pointed out

--- web-app.py
+++ web-app.py
@@ -1,16 +1,3 @@
-FROM registry.access.redhat.com/ubi8/python-38:1-100
-
-WORKDIR /web-app
-
-ADD web-app.py .
-
-RUN pip3 install flask
-
-EXPOSE 5000
-
-CMD ["python3", "./web-app.py"]
-
-[root@ip-172-31-21-153 ~]# cat web-app.py 
 from flask import Flask
 
 app = Flask(__name__)
